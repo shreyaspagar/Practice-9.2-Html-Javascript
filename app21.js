d550ffbafe9181ecf707013ccce694b5bf71a1c3
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:23:53 +0530

    Add files via upload

---
+++ app21.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 18;
+let c = 13;
+
+if (a > b) {
+    if (a > c) {
+        console.log(a, "Is  Largest");
+    }
+    else {
+        console.log(c, "Is  Largest");
+    }
+
+    if (b > c) {
+        console.log(b, "Is Largest");
+    }
+    else {
+        console.log(c, "Is Largest");
+    }
+}
+    
+
+
